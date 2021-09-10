97ab1ce6faff2f61ad536efdadb8971cccb9c4df
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-09-10 13:55:11 +0300

    Added Flowers to sqlite

--- FLOWER/models.py
+++ FLOWER/models.py
@@ -6,4 +6,5 @@ class Flower(models.Model):
     color = models.CharField(max_length=50)
     wellness = models.CharField(max_length=100)
 
-    
+    def __str__(self):
+        return 'I am '+self.color+' Flower'
