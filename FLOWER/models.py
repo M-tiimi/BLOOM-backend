9a95c7eaa3f05e08a6fcf231fbf50361155dbc78
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-09-17 14:08:21 +0300

    updating my project

--- FLOWER/models.py
+++ FLOWER/models.py
@@ -7,4 +7,4 @@ class Flower(models.Model):
     wellness = models.CharField(max_length=100)
 
     def __str__(self):
-        return 'I am '+self.color+' Flower'
+        return 'I am '+self.color+' Flower =)'
