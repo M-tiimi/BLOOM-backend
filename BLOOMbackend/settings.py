c53060bf356044fd925e6b9d9f777068d0c6f220
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-11-10 18:28:35 +0200

    Fix allowed hosts

--- BLOOMbackend/settings.py
+++ BLOOMbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS =  ['bloom-app.azurewebsites.net']
+ALLOWED_HOSTS =  ['bloom-app.azurewebsites.net', 'localhost']
 
 
 # Application definition
