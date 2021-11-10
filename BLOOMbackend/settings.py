1434a415dacba1eddb145baef2280137120882f8
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-11-10 18:26:23 +0200

    Add gitignore + hide secret key

--- BLOOMbackend/settings.py
+++ BLOOMbackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--jthq!^g4%bay%lj^dsw%cv0ln4qb8+(0g9^799paf6p93uol1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS =  ['bloom-app.azurewebsites.net', 'localhost', '192.168.100.3:8000', '127.0.0.1']
+ALLOWED_HOSTS =  ['bloom-app.azurewebsites.net']
 
 
 # Application definition
